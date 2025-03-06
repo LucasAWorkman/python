0e606718e1f4c110572adeec552754f518b97f6d
LucasAWorkman <lucas.workman@bsu.edu>
2025-03-06 16:17:44 +0000

    .

---
+++ main.py
@@ -0,0 +1,18 @@
+import time 
+import random
+import json
+
+
+def
+
+
+def factorial(n):
+    j = n - 1
+
+    while j > 0:
+        n = n*j
+        j -= 1
+    return n
+
+
+print(factorial(15))
---
+++ main.ts
@@ -0,0 +1,8 @@
+
+
+function greet(name : string) : string {
+	return `Hello ${name}`;
+}
+
+let message : string = greet("World");
+console.log(message);
